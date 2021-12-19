c50472b89e7dc65bc73b30e4806018bfbb88da13
JuditAldeguer <jud.vicens@hotmail.es>
2021-12-19 14:57:57 +0100

    update AddToList & Input components

--- src/components/AddToList.tsx
+++ src/components/AddToList.tsx
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import Input from "./Input";
 
 const AddToList = () => {
+  const [input, setInput] = useState({
+    name: "",
+    url: "",
+    age: "",
+    note: "",
+  });
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+  };
   return (
     <div className='AddToList'>
-      <Input placeholder='Name' />
-      <Input placeholder='Age' />
-      <Input placeholder='Image Url' />
-      <textarea className='AddToList-input' placeholder='Notes'>
+      <Input placeholder='Name' input={input} handleChange={handleChange} />
+      <Input placeholder='Age' input={input} handleChange={handleChange} />
+      <Input
+        placeholder='Image Url'
+        input={input}
+        handleChange={handleChange}
+      />
+      <textarea
+        className='AddToList-input'
+        placeholder='Notes'
+        name='Notes'
+        onChange={handleChange}
+      >
         Add
       </textarea>
     </div>
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface IProps {
   placeholder: string;
+  input: {};
+  handleChange: (e) => {};
 }
 
-const Input: React.FC<IProps> = ({ placeholder }) => {
+const Input: React.FC<IProps> = ({ placeholder, input }) => {
   return (
-    <input className='AddToList-input' type='text' placeholder={placeholder}>
+    <input
+      className='AddToList-input'
+      type='text'
+      placeholder={placeholder}
+      name={placeholder.toLowerCase()}
+      value={input.name}
+      onChange={(e) => {}}
+    >
       Add
     </input>
   );
